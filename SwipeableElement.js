601a05ffbc770aac62d99d6f259718457da6396b
Josh Levine <josh.levine1@gmail.com>
2015-04-03 10:29:35 -0700

    Add "pull to" and "release to" helper text when swiping, Animate the element going back to it's start position

--- SwipeableElement.js
+++ SwipeableElement.js
@@ -5,11 +5,14 @@ var {
   View,
   Text,
   PanResponder,
+  LayoutAnimation,
 } = React;
 var Dimensions = require('Dimensions');
 
 var SCREEN_WIDTH = Dimensions.get('window').width;
 var SCREEN_HEIGHT = Dimensions.get('window').height;
+
+var SWIPE_RELEASE_POINT = 70;
 
 var styles = StyleSheet.create({
   swipeableElementWrapper: {
@@ -27,13 +30,13 @@ var styles = StyleSheet.create({
     width: 0,
     backgroundColor: '#FF0000',
     alignItems: 'flex-start',
+    justifyContent:'center',
     //alignSelf: 'flex-start',
   },
   swipeableRight: {
     overflow: 'hidden',
     width: 0,
     alignItems: 'flex-end',
-    //alignSelf: 'flex-end',
     backgroundColor: '#0000FF',
   }
 });
@@ -57,7 +60,6 @@ var SwipeableElement = React.createClass({
       return;
     }
     // The dx value where the gesture should "fire"
-    var activationPoint = 75;
     var dx;
     var direction = gestureState.dx > 0 ? 'right' : 'left';
     if (gestureState.dx < -150) {
@@ -69,32 +71,50 @@ var SwipeableElement = React.createClass({
     }
 
     var absdx = dx > 0 ? dx : -dx;
-    var opacity = (absdx / 75) * 1;
+    var opacity = (absdx / SWIPE_RELEASE_POINT) * 1;
+    var fontSize = 6 + ((opacity > 1 ? 1 : opacity) * 8);
+    var paddingTop = 15 - ((opacity > 1 ? 1 : opacity) * 5);
+    var text;
+    var element;
+
+    var props = { width: absdx*2, opacity, };
 
     this.mainElement.setNativeProps({ left: dx });
-    if (gestureState.dx > 0) {
-      this.leftElement.setNativeProps({ width: dx*2, left: dx, opacity });
+    if (dx > 0) {
+      element = this.leftElement;
+      props.left = absdx;
+      text = this.refs.textLeft;
     } else {
-      this.rightElement.setNativeProps({ width: -dx*2, right: -dx, opacity });
+      element = this.rightElement;
+      props.right = absdx;
+      text = this.refs.textRight;
     }
+    text.setNativeProps({ fontSize, paddingTop });
+    element.setNativeProps(props);
 
     this.setState({ dx });
   },
 
   _handlePanResponderEnd: function() {
-    if (this.state.dx > 75) {
+    if (this.state.dx > SWIPE_RELEASE_POINT) {
       if (this.props.onSwipeRight) {
         this.props.onSwipeRight.call();
       }
-    } else if (this.state.dx < -75) {
+    } else if (this.state.dx < -SWIPE_RELEASE_POINT) {
       if (this.props.onSwipeLeft) {
         this.props.onSwipeLeft.call();
       }
     }
+    var animations = require('../../TaskList/animations');
+    LayoutAnimation.configureNext(animations.easeInEaseOut);
     this.setState({dx:0,});
     this.mainElement && this.mainElement.setNativeProps({ left: 0 });
-    this.leftElement && this.leftElement.setNativeProps({ width: 0 });
-    this.rightElement && this.rightElement.setNativeProps({ width: 0 });
+    this.leftElement && this.leftElement.setNativeProps({ width: 0, left: 0, });
+    this.rightElement && this.rightElement.setNativeProps({ width: 0, right: 0,});
+    setTimeout(() => {
+      this.leftElement && this.leftElement.setNativeProps({ left: null });
+      this.rightElement && this.rightElement.setNativeProps({ right: null });
+    }, 300);
   },
 
   componentWillMount: function() {
@@ -106,21 +126,32 @@ var SwipeableElement = React.createClass({
       onPanResponderRelease: this._handlePanResponderEnd,
       onPanResponderTerminate: this._handlePanResponderEnd,
     });
+
     this.mainElement = (null : ?React.Element);
     this.leftElement = (null : ?React.Element);
     this.rightElement = (null : ?React.Element);
     this.panX = 0;
   },
 
+  getInitialState: function() {
+    return { dx: 0, };
+  },
+
   render: function() {
+    var pullOrRelease = (this.state.dx > SWIPE_RELEASE_POINT || this.state.dx < -SWIPE_RELEASE_POINT) ?
+      'Release' :
+      'Pull';
+
     return (
       <View style={styles.swipeableElementWrapper}>
         <View ref={(element) => {
             this.leftElement = element;
           }} style={styles.swipeableRight}>
-          <View style={{width:300,flexDirection:'row'}}>
-            <View style={{flex:1,backgroundColor:'#0000FF',}}></View>
-            <Text style={{color:'#FFFFFF'}}>{this.props.swipeRightTitle}</Text>
+          <View style={{width:300,flexDirection:'row',}}>
+            <View style={{flex:1,}}></View>
+            <Text ref={'textLeft'} style={{color:'#FFFFFF',padding:10,alignSelf:'center',}}>
+              {pullOrRelease} to {this.props.swipeRightTitle}
+            </Text>
           </View>
         </View>
         <View ref={(element) => {
@@ -132,7 +163,7 @@ var SwipeableElement = React.createClass({
             this.rightElement = element;
           }} style={styles.swipeableLeft}>
           <View style={{width:300,overflow:'hidden'}}>
-            <Text style={{color:'#FFFFFF'}}>{this.props.swipeLeftTitle}</Text>
+            <Text ref={'textRight'} style={{color:'#FFFFFF',padding:10,}}>{pullOrRelease} to {this.props.swipeLeftTitle}</Text>
           </View>
         </View>
       </View>
